c231b6e0eaa0d339e342f2060b1e3bf4372d16d1
Yassine jout <yassinejout@gmail.com>
2018-11-06 13:24:51 +0000

    Update server.js
    Test auto deploy

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var port = process.env.port || 1337;
 
 app.listen(port, function(){
     var datetime = new Date();
-    var message = "Server Test on port " + port ;
+    var message = "Server Test on port 1 " + port ;
     console.log(message);
 });
