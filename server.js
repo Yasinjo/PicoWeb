6adabfe2d19c0d99f16ff0b1a73eb0f2bda2ac7c
Yassine JOUT <yassinejout@gmail.com>
2018-11-06 14:19:58 +0000

    Accessing the app from browser

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 1337;
 
+//Respond with "hello world" for requests that hit our root "/"
+app.get("/", function (req, res) {
+    res.send("hello world");
+   }); 
+
 app.listen(port, function(){
     var datetime = new Date();
     var message = "Server message test" + port ;
