3cd4ea01e0880199def39cd0014dcf1c2aa15754
Yassine jout <yassinejout@gmail.com>
2018-11-06 19:47:52 +0000

    Update server.js
    Adding another json field

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ app.get('/api', (req, res) => {
   res.status(200).send({
     objet : 'Jenkins CI CD msg',
     message: 'Its working Correctly',
-    test : 'test message'
+    test : 'test message',
+    test1 : 'test 2'
 
   })
 });
