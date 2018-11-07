a67b555b56493c15c0e0c5b0e7831f153705950d
Slimane AKALIÂ <slimaneakalia@gmail.com>
2018-11-07 12:07:25 +0100

    Update server.js
    Desx

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/api', (req, res) => {
     message: 'Its working Correctly',
     test : 'test message',
     test1 : 'test 2',
-    project : 'CHantit Project '
+    project : 'CHantit Project 221212512'
 
   })
 });
