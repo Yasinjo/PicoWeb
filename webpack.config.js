0830bb7f863697f44b3bfb99bb7a2af11bbaee12
Slimane AKALIA <slimaneakaliasp@gmail.com>
2019-01-02 20:53:44 +0100

    Adding client assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const {
+  CLIENT_PORT,
+  SERVER_PORT,
+  SERVER_HOST
+} = require('./config/app_config.json');
 
 const outputDirectory = 'dist';
 
+const configDir = path.join(__dirname, 'config');
+const clientDir = path.join(__dirname, 'src', 'client');
+const publicDir = path.join(__dirname, 'public');
+const assetsBase = path.join(publicDir, 'assets');
+
+const cssFiles = [
+  'css/bootstrap-clearmin.css',
+  'css/roboto.css',
+  'css/material-design.css',
+  'css/small-n-flat.css',
+  'css/font-awesome.min.css',
+  'css/style.css'
+];
+
+const jsFiles = [
+  'js/lib/jquery-2.1.3.min.js',
+  'js/jquery.mousewheel.min.js',
+  'js/jquery.cookie.min.js',
+  'js/fastclick.min.js',
+  'js/bootstrap.min.js',
+  'js/clearmin.min.js'
+];
+
+const tabsDir = path.resolve(clientDir, 'Tabs');
+const actionCreatorsDir = path.resolve(clientDir, 'ActionCreators');
+const reducersDir = path.resolve(clientDir, 'Reducers');
+
 module.exports = {
-  entry: './src/client/index.js',
+  entry: path.join(clientDir, '/index.js'),
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
@@ -30,17 +66,55 @@ module.exports = {
     ]
   },
   devServer: {
-    port: 3000,
+    contentBase: assetsBase,
+    port: CLIENT_PORT,
     open: true,
+    historyApiFallback: true,
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api/*': {
+        target: `${SERVER_HOST}:${SERVER_PORT}`,
+        pathRewrite: { '^/api': '' }
+      }
     }
   },
   plugins: [
+    new CopyWebpackPlugin([
+      { from: path.join(assetsBase, 'css'), to: 'css/' },
+      { from: path.join(assetsBase, 'js'), to: 'js/' }
+    ]),
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico'
+      template: path.join(publicDir, 'index.html'),
+      favicon: path.join(publicDir, 'favicon.ico')
+    }),
+    new HtmlWebpackIncludeAssetsPlugin({
+      assets: jsFiles,
+      append: false
+    }),
+    new HtmlWebpackIncludeAssetsPlugin({
+      assets: cssFiles,
+      append: true
     })
-  ]
+  ],
+  performance: {
+    maxAssetSize: 600000,
+    maxEntrypointSize: 600000
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      _config: configDir,
+      _action_creators: actionCreatorsDir,
+      _reducers: reducersDir,
+      _tabs: tabsDir,
+      _home: path.resolve(tabsDir, 'HomeTab'),
+      _codes: path.resolve(tabsDir, 'CodesTab'),
+      _items: path.resolve(tabsDir, 'ItemsTab'),
+      _variables: path.resolve(tabsDir, 'VariablesTab'),
+      _commands: path.resolve(tabsDir, 'CommandsTab'),
+      _shared: path.resolve(tabsDir, 'Shared')
+    }
+  },
+  devtool: 'source-map'
 };
